fb9a7050c17e505fdc0a023f184c0bea8211907f
unibook1283 <uni_book@naver.com>
2021-07-20 22:48:28 +0900

    first commit

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '37952a22de215bf3afe98648c8eaa21c';
+export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/'
